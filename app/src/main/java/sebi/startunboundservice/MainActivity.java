f4be2db80c58c8b0e38920f519c8f38b8011c289
sebi90 <sebwie90@googlemail.com>
2015-06-06 15:57:14 +0200

    Initial commit

---
+++ app/src/main/java/sebi/startunboundservice/MainActivity.java
@@ -0,0 +1,88 @@
+package sebi.startunboundservice;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    private int count;
+    private Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void clickedOnStart(View v) {
+
+        switch (v.getId())
+        {
+            case R.id.buttonStart10:
+                count = 10;
+                Log.d(this.getClass().getName(), "starting service (" +
+                        android.os.Process.myPid() + "-" +
+                        Thread.currentThread().getName() + ")" + "count: " + count);
+                //startService(new Intent(this, MyUnboundService.class).putExtra("count", count));
+                intent = new Intent("sebi.UNBOUND_SERVICE");
+                intent.putExtra("count",count);
+                break;
+
+            case R.id.buttonStart100:
+                count = 100;
+                Log.d(this.getClass().getName(), "starting service (" +
+                        android.os.Process.myPid() + "-" +
+                        Thread.currentThread().getName() + ")" + "count: " + count);
+                //startService(new Intent(this, MyUnboundService.class).putExtra("count", count));
+                intent = new Intent("sebi.UNBOUND_SERVICE");
+                intent.putExtra("count",count);
+                break;
+
+            case R.id.buttonStart1000:
+                count = 1000;
+                Log.d(this.getClass().getName(), "starting service (" +
+                        android.os.Process.myPid() + "-" +
+                        Thread.currentThread().getName() + ")" + "count: " + count);
+                //startService(new Intent(this, MyUnboundService.class).putExtra("count", count));
+                intent = new Intent("sebi.UNBOUND_SERVICE");
+                intent.putExtra("count",count);
+                break;
+
+        }
+
+        intent.setPackage("sebi.unboundservice");
+        startService(intent);
+
+    }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
